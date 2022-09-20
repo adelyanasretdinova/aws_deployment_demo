afead5f658ae6c5ffdb06b374387679db3531280
Nathalie Scherf <nascherf@yahoo.se>
2022-09-20 13:36:29 +0200

    comments on style and repeating code

--- my-wardrobe/src/App.js
+++ my-wardrobe/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Header from './components/Header';
 import Card from './components/Card';
-
+// Include Bootstrap
+// Array of card items (objects)
+// Function to repeat 3 times: map
+// {} switch to JS in the return function
 const App = () => {
   return (
     <div className="App">
       {/* Use the component */}
       <Header />
+      <Card />    
+      <Card />    
       <Card />    
       </div>
   );
--- my-wardrobe/src/components/Card.js
+++ my-wardrobe/src/components/Card.js
@@ -1,13 +1,15 @@
 const Card = () => {
   return (
-    <div className="card" style={{ width: "18rem" }}>
-
-            <div className="card-body">
-              <h5 className="card-title">T-shirt</h5>
-              <p className="card-text">  A nice cotton T-Shirt  </p>
-              <span className="btn btn-primary" id='T-shirt'  >Add to outfit</span>
-            </div>
-          </div>
+    <>
+    <div className="card" style={{ width: "18rem", border: "solid" }}>
+      <div className="card-body">
+        <h5 className="card-title">T-shirt</h5>
+        <p className="card-text">  A nice cotton T-Shirt  </p>
+        <button className="btn btn-primary" id='T-shirt'  >Add to outfit</button>
+      </div>
+    </div>
+    <p>This will not break it!</p>
+    </>
   )
 }
 
