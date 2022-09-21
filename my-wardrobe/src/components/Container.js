41b5da3034a3d5fe0070bcd9585f0a9371f821d1
Nathalie Scherf <nascherf@yahoo.se>
2022-09-21 16:14:00 +0200

    use outfit and add state for outfit

--- my-wardrobe/src/components/Container.js
+++ my-wardrobe/src/components/Container.js
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 import Wardrobe from "./Wardrobe"
 import { tshirt, socks, shorts } from '../mockData'
+import Outfit from './Outfit'
 
 const Container = () => {
   // Here we will add state! We use the useState Hook
   // the data, and a function to change the data:
   const [ wardrobe, setWardrobe ] = useState([ tshirt, shorts, socks ])
-
+  const [outfit, setOutfit ] = useState([ tshirt ])
   return(
   <div className='Container'>
     <Wardrobe wardrobeData={wardrobe}/>
+    <Outfit outfitData={outfit}></Outfit>
   </div>)
 }
 
