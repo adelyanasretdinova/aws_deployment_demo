df19ac000a3b3436084bcb95027959ab6b2b81fd
Nathalie Scherf <nascherf@yahoo.se>
2022-09-21 15:53:19 +0200

    style outfit card

--- my-wardrobe/src/components/OutfitCard.js
+++ my-wardrobe/src/components/OutfitCard.js
@@ -1,12 +1,10 @@
 const OutfitCard = (props) => {
   return (
-    <>
     <div className="Outfitcard card" style={{ width: "18rem", border: "solid" }}>
       <div className="card-body">
         <h5 className="card-title">{props.item.descrShort}</h5>      
       </div>
     </div>
-    </>
   )
 }
 
