fd15b7a0e9e843f719c12cf12cd9ab8a60c4018f
Nathalie Scherf <nascherf@yahoo.se>
2022-09-23 09:56:31 +0200

    display text depending on length of outfit array

--- my-wardrobe/src/components/Outfit.js
+++ my-wardrobe/src/components/Outfit.js
@@ -1,9 +1,23 @@
 import OutfitCard from "./OutfitCard"
 
 const Outfit = (props) => {
+  let text;
+
+  if(props.outfitData.length > 0) {
+    text = "This is your styling for today:"
+  } else {
+    text =  "Select an outfit!"
+  }
+
   return (
     <div className="Outfit">
-      <h5>This is your styling for today:</h5>
+      <h3>{text}</h3>
+      {/* Check length, display different header */}
+      { (props.outfitData.length > 0) ? ( 
+        <h3>This is your styling for today:</h3>
+       ) : ( 
+          <h3> Select an outfit! </h3> )
+      } 
       <div className="d-flex">
         {props.outfitData.map((element, index) => {
           return <OutfitCard key={index} item={element} removeFromOutfit={props.removeFromOutfit}/>
