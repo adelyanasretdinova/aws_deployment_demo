aab6765d5acdef2e9818176c16734d3cd15bb4b0
Nathalie Scherf <nascherf@yahoo.se>
2022-09-27 09:56:18 +0200

    add routing and an about route

--- my-wardrobe/src/App.js
+++ my-wardrobe/src/App.js
@@ -1,13 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './App.css';
 import Header from './components/Header';
 import Container from './components/Container';
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <Container />
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          {/* display container on / */}
+          <Route path='/' element={<Container />} />
+          {/* Display a new about- component on /about */}
+          <Route path='/about' element={<div>This is my about page. Need to create a component for this!</div>} />
+          <Route path='*' element={<div> Page not found</div>} />
+
+        </Routes>
       </div>
+    </BrowserRouter>
   );
 }
 
--- my-wardrobe/src/components/Header.js
+++ my-wardrobe/src/components/Header.js
@@ -1,8 +1,16 @@
+import { Link } from "react-router-dom"
+
 // Create what we should see: html and js
 const Header = () => {
   return (
     <header>
       <h1> Welcome to my online wardrobe!</h1>
+      <nav> 
+        <ul>
+        <li> <Link to="/">Main page wardrobe</Link></li>
+        <li> <Link to="/about">About</Link></li>
+        </ul>
+      </nav>
     </header>
   )
 }
