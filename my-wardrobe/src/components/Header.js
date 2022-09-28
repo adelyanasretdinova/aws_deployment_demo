8acc3990ffcb0a364d9ddacf545d315aeb210640
Nathalie Scherf <nascherf@yahoo.se>
2022-09-28 10:00:46 +0200

    add styling to navbar

--- my-wardrobe/src/components/Header.js
+++ my-wardrobe/src/components/Header.js
@@ -1,14 +1,13 @@
 import { Link } from "react-router-dom"
 
-// Create what we should see: html and js
 const Header = () => {
   return (
     <header>
       <h1> Welcome to my online wardrobe!</h1>
-      <nav> 
-        <ul>
-        <li> <Link to="/">Main page wardrobe</Link></li>
-        <li> <Link to="/about">About</Link></li>
+      <nav className="d-flex justify-content-center"> 
+        <ul className="nav">
+        <li className="nav-item"> <Link to="/" className="nav-link">Main page wardrobe</Link></li>
+        <li className="nav-item"> <Link to="/about" className="nav-link">About</Link></li>
         </ul>
       </nav>
     </header>
