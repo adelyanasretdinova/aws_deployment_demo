da7f057fdd2c846be4d66315dc9bb6db76e9a156
Nathalie Scherf <nascherf@yahoo.se>
2022-09-28 11:01:16 +0200

    add border to navbar

--- my-wardrobe/src/components/Header.js
+++ my-wardrobe/src/components/Header.js
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <header>
       <h1> Welcome to my online wardrobe!</h1>
-      <nav className="d-flex justify-content-center"> 
-        <ul className="nav">
+      <nav className="d-flex justify-content-center "> 
+        <ul className="nav border-bottom border-primary">
         <li className="nav-item"> <Link to="/" className="nav-link">Main page wardrobe</Link></li>
         <li className="nav-item"> <Link to="/about" className="nav-link">About</Link></li>
         </ul>
