a9a9cf5a7ad2d64ed4bf426abcfa1b7c3e38c2bd
Nathalie Scherf <nascherf@yahoo.se>
2022-10-05 10:15:42 +0200

    refactor container

--- my-wardrobe/src/components/Container.js
+++ my-wardrobe/src/components/Container.js
@@ -11,65 +11,58 @@ const Container = () => {
   const [weather, setWeather] = useState({})
   const SEASONS = ['summer', 'fall', 'winter', 'spring']
 
-  useEffect(() => {
-    const fetchWardrobe = async () => {
-      try{
-      // fetch
-      let path = 'http://localhost:8000/wardrobe'
+  const fetchWardrobe = async () => {
+    try{
+    let path = `${process.env.REACT_APP_WARDROBE_API}/wardrobe`
+    let response = await fetch(path, { mode: 'cors' })
+    let fetchedWardrobeData = await response.json()
+    let dataToStore = fetchedWardrobeData.data.map(item => ({...item, url: `http://localhost:8000${item.url}`}))
+    setWardrobe(dataToStore)
+    } catch(error) {
+      console.log('Something went wrong fetching data', error);
+    }
+  }
+
+  const fetchWeather = async () => {
+    try {
+      let path = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Berlin?unitGroup=metric&key=${process.env.REACT_APP_VISUAL_KEY}&contentType=json`
       let response = await fetch(path, { mode: 'cors' })
-      // inspect the data
-      let fetchedWardrobeData = await response.json()
-      // console.log(fetchedWardrobeData.data)
-      // each item: Change url of image to `http://localhost:8000/images/tshirt.jpg`
-        let dataToStore = fetchedWardrobeData.data.map(item => {
-          return {...item, url: `http://localhost:8000${item.url}` }
-        })
-      // save data to state
-        setWardrobe(dataToStore)
-
-      } catch(error) {
-        console.log('Something went wrong fetching data', error);
-      }
-      
+      let data = await response.json()
+      let city = data.address;
+      let temperature = data.currentConditions.temp;
+      let conditions = data.currentConditions.conditions
+      conditions = conditions.split(' ').map(word => word.toLowerCase()).join(' ')
+      setWeather({ city, conditions, temperature })
     }
-    fetchWardrobe()
-
-    const fetchData = async () => {
-      try {
-        let path = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Berlin?unitGroup=metric&key=${process.env.REACT_APP_VISUAL_KEY}&contentType=json`
-        let response = await fetch(path, { mode: 'cors' })
-        let data = await response.json()
-
-        let city = data.address;
-        let temperature = data.currentConditions.temp;
-        let conditions = data.currentConditions.conditions
-        conditions = conditions.split(' ').map(word => word.toLowerCase()).join(' ')
-
-        let myWeatherData = { city, conditions, temperature }
-        setWeather(myWeatherData)
-      }
-      catch (error) {
-        console.log("There was an error when fetching data", error);
-      }
+    catch (error) {
+      console.log("There was an error when fetching data", error);
     }
-    
-    fetchData()
-    // fetch data from the backend and pass to state!
+  }
 
+  const getDataFromLS = () => {
     let outfitJson = localStorage.getItem('outfitLS')
     let outfitParsed = JSON.parse(outfitJson)
     if (outfitParsed) {
       setOutfit(outfitParsed)
     }
+  }  
+
+  const saveDataToLS = () => {
+    localStorage.setItem('outfitLS', JSON.stringify(outfit))
+  }
+
+  useEffect(() => {
+    fetchWardrobe()
+    fetchWeather()
+    getDataFromLS()
   }, [])
 
   useEffect(() => {
-    localStorage.setItem('outfitLS', JSON.stringify(outfit))
+    saveDataToLS()
   }, [outfit])
 
   const addToOutfit = (event) => {
     let id = event.target.id
-    console.log("ID of item", id);
     let clickedItem = wardrobe.find(item => item.id === id)
     setOutfit([...outfit, clickedItem])
   }
